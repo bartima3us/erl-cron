d23fcf367c65e6f1adaf3374c180640a640dc765
Šarūnas <sarunas0919@gmail.com>
2018-06-15 02:09:52 +0300

    Parser tests.

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -7,19 +7,18 @@
 %%% Created : 07. Jun 2017 08.49
 %%%-------------------------------------------------------------------
 -module(basic_SUITE).
--include_lib("eunit/include/eunit.hrl").
 -author("sarunas").
 
 %% API
--export([all/0]).
--export([test1/1, test2/1]).
-
-all() -> [test1,test2].
-
-test1(_Config) ->
-  1 = 1.
-
-test2(_Config) ->
-  A = 0,
-  1/A.
+%%-export([all/0]).
+%%-export([test1/1, test2/1]).
+%%
+%%all() -> [test1,test2].
+%%
+%%test1(_Config) ->
+%%  1 = 1.
+%%
+%%test2(_Config) ->
+%%  A = 0,
+%%  1/A.
 
